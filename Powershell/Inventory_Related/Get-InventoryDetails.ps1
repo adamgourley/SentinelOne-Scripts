ae794cf0f02a90f069438df29601cd1d5b7f6ec3
adamgourley <104019246+adamgourley@users.noreply.github.com>
2022-09-06 12:16:04 -0400

    Add export to CSV functionality

--- Powershell/Inventory_Related/Get-InventoryDetails.ps1
+++ Powershell/Inventory_Related/Get-InventoryDetails.ps1
@@ -3,8 +3,26 @@
 # Author(s): Adam Gourley
 # RSO Type: Artifact Collection
 
+<#
+
+.SYNOPSIS
+Gets a variety of information from the computer and returns it as a PS Object, or exports it to a CSV.
+
+.PARAMETER Path
+Takes a path to use for CSV export.
+
+.EXAMPLE
+Export the information to a CSV file.
+.\Get-InventoryDetails.ps1 -Path .\inventory.csv
+
+.EXAMPLE
+Only return the information to the shell.
+.\Get-InventoryDetails.ps1
+
+#>
+
 param(
-    [Parameter(Mandatory = $true,
+    [Parameter(Mandatory = $false,
         ParameterSetName = "Path",
         HelpMessage = "Enter the CSV output file path",
         Position = 0)]
@@ -13,6 +31,7 @@ param(
 
 function InventoryDetails {
     [PSCustomObject]@{
+        "DateInfoCollected" = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
         "ComputerName" = hostname.exe
         "Manufacturer" = Get-WmiObject -Query "select * from Win32_ComputerSystem" | Select-Object -ExpandProperty Manufacturer
         "Domain" = Get-WmiObject -Query "select * from Win32_ComputerSystem" | Select-Object -ExpandProperty Domain
@@ -22,10 +41,19 @@ function InventoryDetails {
         "ProcessorName" = Get-CimInstance -ClassName Win32_Processor | Select-Object -ExcludeProperty "CIM*" | Select-Object -ExpandProperty Name
         "ProcessorCount" = Get-CimInstance -ClassName Win32_Processor |  Select-Object -ExpandProperty NumberOfCores
         "HostOS" = (Get-CimInstance -ClassName CIM_OperatingSystem).Caption
-        "AvailableSpace" = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Measure-Object -Property Size -Sum | Select-Object -Property @{Expression={$_.Sum / 1GB};Label="Sum"} | Select-Object -ExpandProperty Sum
+        "OSBuild" = (Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID')
+        "AvailableSpace" = Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='$env:systemdrive'" | ForEach-Object { [math]::Round($_.Size / 1GB,2) }
         "BIOSVersion" = Get-CimInstance -ClassName Win32_Bios | Select-Object -ExpandProperty SMBIOSBIOSVersion
-        # "LoggedOnUser" = Get-CimInstance -ClassName Win32_ComputerSystem -Property UserName | Select-Object -ExpandProperty UserName
+        "IPAddress" = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $null -ne $_.DefaultIPGateway }).IPAddress | Select-Object -First 1
+        "MACAddress" = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $null -ne $_.DefaultIPGateway } | Select-Object -ExpandProperty MACAddress
+        "LoggedOnUser" = Get-CimInstance -ClassName Win32_ComputerSystem -Property UserName | Select-Object -ExpandProperty UserName
     }
 }
 
-Export-CSV -Path $Path -InputObject InventoryDetails
+if ($Path) {
+    <# If path is supplied, export to CSV. #>
+    InventoryDetails | Select-Object * | Export-CSV -Path $Path
+} else {
+    <# Action when all if and elseif conditions are false #>
+    return InventoryDetails
+}
